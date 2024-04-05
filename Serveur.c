77f4d4af70d5734bb98f827685b00efd0ac14dc1
deniel.hedbaut <deniel.hedbaut@igtp5-pc15.isim.intra>
2024-04-05 11:19:53 +0200

    Server code update

--- Serveur.c
+++ Serveur.c
@@ -8,26 +8,50 @@
 
 
 int main(int argc, char *argv[]) {
-    printf("Début programme\n");
 
-    int dS = socket(PF_INET, SOCK_STREAM, 0);
-    printf("Socket créé\n");
-
-    struct sockaddr_in ad;
-    ad.sin_family = AF_INET;
-    ad.sin_addr.s_addr = INADDR_ANY;
-    ad.sin_port = htons(atoi(argv[1]));
-    int connect = bind(dS, (struct sockaddr*)&ad, sizeof(ad)); //variable "connect" to avoid conflict with the function connect
-    if (connect < 0) {
-        printf("Connexion error : bind failed\n");
+    if (argc != 2) { //security : check if the number of arguments is correct
+        printf("Number of arguments incorrect\n");
+        printf("Usage : %s <port>\n", argv[0]);
         return -1;
     }
-    printf("Socket nommé\n");
+
+    printf("Start program\n");
+
+    int dS = socket(PF_INET, SOCK_STREAM, 0); //keep the socket descriptor (id of the socket)
+    printf("Socket created\n");
+
+    struct sockaddr_in ad;
+    ad.sin_family = AF_INET; //address family
+    ad.sin_addr.s_addr = INADDR_ANY; //address to accept any incoming messages
+    ad.sin_port = htons(atoi(argv[1])); //port passed as argument
+    int connect = bind(dS, (struct sockaddr*)&ad, sizeof(ad)); //variable "connect" to avoid conflict with the function connect
+    if (connect < 0) {
+        printf("Connection error: bind failed\n");
+        return -1;
+    }
+    printf("Socket named\n");
 
     int ecoute = listen(dS, 7); //variable "ecoute" to avoid conflict with the function listen
     if (ecoute < 0) {
-        printf("Connexion error : listen failed\n");
+        printf("Connection error: listen failed\n");
         return -1;
     }
-    printf("Mode écoute\n");
+    printf("Listening mode\n");
+
+    struct sockaddr_in aC ;
+    socklen_t lenght = sizeof(struct sockaddr_in); //keep the length of the address
+    int dSC = accept(dS, (struct sockaddr*) &aC, &lenght); //keep the (new) socket descriptor of the client
+    if (dSC == -1) {
+        printf("Connection error: client failed to connect\n");
+        return -1;
+    }
+    printf("\n--Client connected--\n\n");
+    
+    char msg [300]; //message buffer : 300 characters max
+
+    int rec = recv(dSC, msg, sizeof(msg), 0);
+    msg[rec] = '\0';
+    printf("Message received : %s\n", msg);
+
+
 }
