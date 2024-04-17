9b4191fda63f171d1c6800dfff1233dc7d8a6ee9
mathias <mathias.puyfages@etu.umontpellier.fr>
2024-04-17 20:06:14 +0200

    Modify of architecture, Mise en place d'un fonctionnement en thread du serveur

--- Serveur.c
+++ Serveur.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
 
 //Command to launch this program : ./Serveur port
@@ -12,6 +13,10 @@
 This program should act as a relay of message 
 This should start the conversation when 2 Clients are connected, capture the senders and the receiver and switch them, and end when "fin" is sended by someone
 */
+struct thread_argument {
+    int descripteur;
+    int* tab_of_client;
+};
 
 int creation_socket() {
         int dS = socket(PF_INET, SOCK_STREAM, 0);
@@ -38,6 +43,45 @@ int make_bind(int socket,struct sockaddr_in adresse) {
     printf("Socket named\n");
 }
 
+int connect_to_client (struct sockaddr_in adress, int descripteur) {
+    socklen_t lenght = sizeof(struct sockaddr_in); //keep the length of the address
+    int dSClient = accept(descripteur, (struct sockaddr*) &adress, &lenght); //keep the (new) socket descriptor of the client
+    if (dSClient == -1) {
+        perror("Connection error: client failed to connect\n");
+        return -1;
+    }
+    printf("\n-- Client connected --\n");
+    return dSClient;
+}
+
+void * discussion (void * arg) {
+    struct thread_argument * argument = (struct thread_argument *) arg;
+    short conversation = 1;
+    char message[300];
+    int receiver;
+    if (argument->tab_of_client[0] == argument->descripteur) {
+        receiver = argument->tab_of_client[1];
+    }
+    else {
+        receiver = argument->tab_of_client[0];
+    }
+    while (conversation) {
+        int res =  recv(argument->descripteur, message, sizeof(char)*300, 0);
+        printf("message recu : %s \n",message);
+        if (res < 0) {
+            perror("Error receiving the message");
+                exit(0);
+        }
+
+        res = send(receiver, message, sizeof(char)*300 , 0);
+        if (res < 0) {
+            perror("Error sending the message");
+            exit(0);
+        }
+        sleep(0.01);
+}
+}
+
 int main(int argc, char *argv[]) {
 
     if (argc != 2) { //security : check if the number of arguments is correct
@@ -58,6 +102,8 @@ int main(int argc, char *argv[]) {
 
     int connect = make_bind(dS,adresse);
 
+    int tab_client[2]; 
+
     /*
     One of the problem is to find who is to konow who is the sender and who is the receiver for the start, given that this is alternante next
     The idea here is that the both clients send their type to be affected the correct role
@@ -66,7 +112,6 @@ int main(int argc, char *argv[]) {
    //This loop should be running unless there is an external interruption, because this allow to handle the connection of new Clients when the last conversation has ended
     while (running) {
 
-        //Get ready for receiving the type of the client
         int ecoute = listen(dS,1);
         if (ecoute < 0) {
             perror("Connection error: listen failed\n");
@@ -74,68 +119,26 @@ int main(int argc, char *argv[]) {
         }
         printf("Listening mode\n");
 
-        struct sockaddr_in aC ;
-        socklen_t lenght = sizeof(struct sockaddr_in); //keep the length of the address
-        int dSClient1 = accept(dS, (struct sockaddr*) &aC, &lenght); //keep the (new) socket descriptor of the client
-        if (dSClient1 == -1) {
-            perror("Connection error: client failed to connect\n");
-            return -1;
-        }
-        printf("\n-- Client connected 1 --\n\n");
+        struct sockaddr_in aC1 ;
+        struct sockaddr_in aC2 ;
 
-        struct sockaddr_in aD ;
-        socklen_t lenght1 = sizeof(struct sockaddr_in); //keep the length of the address
-        int dSClient2 = accept(dS, (struct sockaddr*) &aD, &lenght1); //keep the (new) socket descriptor of the client
-        if (dSClient2 == -1) {
-            perror("Connection error: client failed to connect\n");
-            return -1;
-        }
-        printf("\n-- Client 2 connected --\n\n");
+        tab_client[0] = connect_to_client(aC1,dS);
+        tab_client[1] = connect_to_client(aC2,dS);
 
-        int dSClientReceiver = dSClient2;
+        pthread_t tid;
+        pthread_t tid2;
 
-        int dSClientSender = dSClient1;
+        struct thread_argument arg1 = {tab_client[0],tab_client};
+        struct thread_argument arg2 = {tab_client[1],tab_client};
 
         printf("Initialisation réussi \n") ;
-        short conversation = 1;
-        char message[300];
-        while (conversation) {
-            printf("En attente d'un message \n");
-            int res =  recv(dSClientSender, message, sizeof(char)*300, 0);
-            printf("message recu : %s \n",message);
-            if (res < 0) {
-                perror("Error receiving the message");
-                exit(0);
-            }
 
-            res = send(dSClientReceiver, message, sizeof(char)*300 , 0);
-            if (res < 0) {
-                perror("Error receiving the message");
-                exit(0);
-            }
+        int i = pthread_create (&tid, NULL, discussion,&arg1);
+        int j = pthread_create(&tid2,NULL,discussion,&arg2);
 
-            if (strcmp(message,"fin")==0) {
-                int fermeture;
-                fermeture = close(dSClientSender);
-                if (fermeture < 0) {
-                    perror("Error when closing dSClient1");
-                }
-                fermeture = close(dSClient2);
-                if (fermeture < 0) {
-                    perror("Error when closing dSClient2");
-                }
-                printf("Conversation fermé\n");
-                conversation = 0;
-            }
-            else {
-                int dSechange = dSClientReceiver;
-                dSClientReceiver = dSClientSender;
-                dSClientSender = dSechange; 
-            }
+        //Waiting for the close of the 2 threads 
+        pthread_join(tid,NULL);
+        pthread_join(tid2,NULL);
 
-            sleep(1);
-        }
-        
-    }
-
+}
 }
