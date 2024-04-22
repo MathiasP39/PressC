71fd5ed84c8efc0478143029f2443382451e7ef5
deniel.hedbaut <deniel.hedbaut@igtp5-pc15.isim.intra>
2024-04-22 17:50:05 +0200

    Gestion d'erreurs, suppression des deux clients obligatoires dans le main, doc, minor changes

--- Serveur.c
+++ Serveur.c
@@ -9,39 +9,73 @@
 
 //Command to launch this program : ./Serveur port
 // Exemple : ./Serveur 3500
-/*
-This program should act as a relay of message 
-This should start the conversation when 2 Clients are connected, capture the senders and the receiver and switch them, and end when "fin" is sended by someone
-*/
+
+
+/**
+ * @struct thread_argument
+ * @brief Structure representing the arguments passed to a thread function.
+ * 
+ * This structure contains the file descriptor of the client connection and an array of client descriptors.
+ */
 struct thread_argument {
-    int descripteur;
-    int* tab_of_client;
+    int descripteur;        /**< The file descriptor of the client connection */
+    int* tab_of_client;     /**< An array of client descriptors */
 };
 
+/**
+ * @struct arg_get_client
+ * @brief Structure representing the arguments for getting clients.
+ * 
+ * This structure holds the necessary information for getting clients in the server.
+ * It includes an array of client IDs, the maximum number of clients, and the server socket descriptor.
+ */
 struct arg_get_client {
-        int* tab_client;
-        int Nb_client_max;
-        int dS;
+    int* tab_client;        /**< Array of client IDs */
+    int Nb_client_max;      /**< Maximum number of clients */
+    int dS;                 /**< Server socket descriptor */
 };
 
+
+/**
+ * Creates a socket for TCP/IP communication.
+ *
+ * @return The socket file descriptor if successful, -1 otherwise.
+ */
 int creation_socket() {
-        int dS = socket(PF_INET, SOCK_STREAM, 0);
-        printf("Socket created \n");
+    int dS = socket(PF_INET, SOCK_STREAM, 0);
+    if (dS == -1) {
+        perror("Connection error: socket creation failed\n");
         return dS;
+    }
+    printf("Socket created \n");
+    return dS;
 }
 
-struct sockaddr_in param_socket_adresse(char * port) {
-    //Get the adresses of the first Client
+/**
+ * Creates and returns a socket address structure based on the given port number.
+ *
+ * @param port The port number to be used for the socket address.
+ * @return The created socket address structure.
+ */
+struct sockaddr_in param_socket_adresse(char *port) {
+    // Get the addresses of the first Client
     struct sockaddr_in adresse1;
-    adresse1.sin_family = AF_INET; //address family
-    adresse1.sin_addr.s_addr = INADDR_ANY; //address to accept any incoming messages
-    adresse1.sin_port = htons(atoi(port)); //port passed as argument
+    adresse1.sin_family = AF_INET; // address family
+    adresse1.sin_addr.s_addr = INADDR_ANY; // address to accept any incoming messages
+    adresse1.sin_port = htons(atoi(port)); // port passed as argument
     printf("Adresse creation \n");
     return adresse1;
 }
 
-int make_bind(int socket,struct sockaddr_in adresse) {
-    int connect = bind(socket,(struct sockaddr*)&adresse,sizeof(adresse)); //variable "connect" to avoid conflict with the function connect
+/**
+ * Binds a socket to a specific address.
+ *
+ * @param socket The socket to bind.
+ * @param adresse The address to bind the socket to.
+ * @return Returns 0 on success, or an error code on failure.
+ */
+int make_bind(int socket, struct sockaddr_in adresse) {
+    int connect = bind(socket, (struct sockaddr*)&adresse, sizeof(adresse)); //variable "connect" to avoid conflict with the function connect
     if (connect != 0) {
         printf("Connection error: bind failed\n");
         return connect;
@@ -49,7 +83,17 @@ int make_bind(int socket,struct sockaddr_in adresse) {
     printf("Socket named\n");
 }
 
-int connect_to_client (struct sockaddr_in adress, int descripteur) {
+/**
+ * Connects to a client.
+ * 
+ * This function accepts a client connection on the given socket descriptor.
+ * It returns the new socket descriptor of the connected client.
+ * 
+ * @param adress The server address structure.
+ * @param descripteur The socket descriptor of the server.
+ * @return The new socket descriptor of the connected client, or -1 if an error occurred.
+ */
+int connect_to_client(struct sockaddr_in adress, int descripteur) {
     socklen_t lenght = sizeof(struct sockaddr_in); //keep the length of the address
     int dSClient = accept(descripteur, (struct sockaddr*) &adress, &lenght); //keep the (new) socket descriptor of the client
     if (dSClient == -1) {
@@ -60,6 +104,13 @@ int connect_to_client (struct sockaddr_in adress, int descripteur) {
     return dSClient;
 }
 
+/**
+ * Sends a message to all clients except the sender.
+ * 
+ * @param socket_sender The socket descriptor of the sender.
+ * @param message The message to send.
+ * @param tab_client The array of client socket descriptors.
+ */
 void send_all(int socket_sender, char *message, int *tab_client) {
     for (int i = 0; tab_client[i] != -1; i++) {
         if (tab_client[i] != socket_sender) {
@@ -68,6 +119,13 @@ void send_all(int socket_sender, char *message, int *tab_client) {
     }
 }
 
+/**
+ * Handles the conversation between two clients.
+ * 
+ * This function is responsible for managing the communication between two clients. It receives messages from one client and sends them to the other client.
+ * 
+ * @param arg The argument structure containing the socket descriptor and the array of client socket descriptors.
+ */
 void * discussion (void * arg) {
     struct thread_argument * argument = (struct thread_argument *) arg;
     short conversation = 1;
@@ -90,10 +148,20 @@ void * discussion (void * arg) {
     }
 }
 
-int add_client (int * tab_client,int size,int dS) {
+/**
+ * Adds a client to the client array.
+ *
+ * This function searches for an available slot in the client array and adds the client socket descriptor to it.
+ *
+ * @param tab_client The client array.
+ * @param size The size of the client array.
+ * @param dS The client socket descriptor to be added.
+ * @return Returns 0 if the client was successfully added, -1 otherwise.
+ */
+int add_client(int *tab_client, int size, int dS) {
     int res = -1;
     int i = 0;
-    while (res == -1 && i<size){
+    while (res == -1 && i < size) {
         if (tab_client[i] == -1) {
             tab_client[i] = dS;
             res = 0;
@@ -103,6 +171,16 @@ int add_client (int * tab_client,int size,int dS) {
     return res;
 }
 
+/**
+ * Gets a client connection.
+ *
+ * This function is responsible for handling the connection of new clients to the server.
+ * It continuously accepts new client connections and adds them to the client array.
+ * If the maximum number of clients is reached, it sends a message to the client indicating that the server is full.
+ * If a client is successfully connected, it creates a new thread to handle the conversation with the client using the "discussion" function.
+ *
+ * @param arg The argument structure containing the client array, the maximum number of clients, and the server socket descriptor.
+ */
 void * get_client (void * arg ) {
     struct arg_get_client *args = (struct arg_get_client *) arg;
     while (1) {
@@ -112,22 +190,29 @@ void * get_client (void * arg ) {
         if (res == -1) {
             char message[] = "You can't connect there is already too many people connected, retry later";
             send(dSClient, message, sizeof(message) , 0);
-        close(dSClient);
+            close(dSClient);
         }
         else if (res == 0) {
             pthread_t tid;
             struct thread_argument arg = {dSClient,args->tab_client};
-            int i = pthread_create (&tid, NULL, discussion,&arg);
+            int i = pthread_create (&tid, NULL, discussion, &arg);
         }
     }
 } 
 
+
+/**
+ * @brief The main function of the server program.
+ * 
+ * @param argc The number of command-line arguments.
+ * @param argv An array of strings containing the command-line arguments.
+ * @return 0 on success, -1 on failure.
+ */
 int main(int argc, char *argv[]) {
 
     if (argc != 2) { //security : check if the number of arguments is correct
         perror("Incorrect number of arguments");
         printf("Usage : %s <port>\n", argv[0]);
-        exit(0);
         return -1;
     }
 
@@ -138,10 +223,17 @@ int main(int argc, char *argv[]) {
     short running = 1;    
 
     int dS = creation_socket();
+    if (dS == -1) {
+        return -1;
+    }
 
     struct sockaddr_in adresse = param_socket_adresse(argv[1]);
 
     int connect = make_bind(dS,adresse);
+    if (connect != 0) {
+        close(dS);
+        return -1;
+    }
 
     int tab_client[NB_CLIENT_MAX];
 
@@ -153,45 +245,31 @@ int main(int argc, char *argv[]) {
     int ecoute = listen(dS,1);
     if (ecoute < 0) {
         perror("Connection error: listen failed\n");
+        close(dS);
         return ecoute;
     }
+    
     printf("Listening mode\n");
 
 
     /*
-    One of the problem is to find who is to konow who is the sender and who is the receiver for the start, given that this is alternante next
+    One of the problem is to find who is to know who is the sender and who is the receiver for the start, given that this is alternante next
     The idea here is that the both clients send their type to be affected the correct role
     */
-   //The part above is 
-   //This loop should be running unless there is an external interruption, because this allow to handle the connection of new Clients when the last conversation has ended
-    while (running) {
-        struct sockaddr_in aC1 ;
-        struct sockaddr_in aC2 ;
 
-        //The conversation start when at least to client are connected 
-        tab_client[0] = connect_to_client(aC1,dS);
-        tab_client[1] = connect_to_client(aC2,dS);
+    //Creating the thread that will handle the connection of new client
 
-        pthread_t tid;
-        pthread_t tid2;
-        pthread_t thread_add_client;
+    pthread_t thread_add_client;
 
-        struct thread_argument arg1 = {tab_client[0],tab_client};
-        struct thread_argument arg2 = {tab_client[1],tab_client};
+    struct arg_get_client arg_client = {tab_client,NB_CLIENT_MAX,dS};
 
-        printf("Initialisation réussi \n") ;
-        
+    int k = pthread_create(&thread_add_client, NULL, get_client, &arg_client);
 
-        int i = pthread_create (&tid, NULL, discussion,&arg1);
-        int j = pthread_create(&tid2,NULL,discussion,&arg2);
+    //Waiting for the close of the thread
 
-        //Creating the thread that will handle the connection of new client 
-        struct arg_get_client arg_client = {tab_client,NB_CLIENT_MAX,dS};
+    pthread_join(thread_add_client,NULL);
 
-        int k = pthread_create(&thread_add_client,NULL,get_client,&arg_client);
+    close(dS);
 
-        //Waiting for the close of the 2 threads 
-        pthread_join(tid,NULL);
-        pthread_join(tid2,NULL);
-}
+    return 0;
 }
